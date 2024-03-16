ea7c03e55dcad0c81ddb70ca89d82cc4b7769c8d
rrubaszek <robert.rubaszek@gmail.com>
2024-03-16 19:21:31 +0100

    wrapper for ada

--- lista1/ada/iterative.ads
+++ lista1/ada/iterative.ads
@@ -1,9 +1,21 @@
 package Iterative is
     
-    function Factorial(N : Integer) return Integer;
+    function Factorial(N : Integer) return Integer
+        with 
+            Export        => True,
+            Convention    => C,
+            External_Name => "factorial_c";
 
-    function GCD(A, B : Integer) return Integer;
+    function GCD(A, B : Integer) return Integer
+        with 
+            Export        => True,
+            Convention    => C,
+            External_Name => "gcd_c";
 
-    procedure Diophantine(A, B, C : Integer; X, Y : out Integer);
+    procedure Diophantine(A, B, C : Integer; X, Y : out Integer)
+        with 
+            Export        => True,
+            Convention    => C,
+            External_Name => "diophantine_c";
 
 end Iterative;
--- lista1/ada/recursive.ads
+++ lista1/ada/recursive.ads
@@ -1,9 +1,23 @@
+with interfaces.C; use interfaces.C;
+
 package Recursive is
 
-    function Factorial(N : Integer) return Integer;
+    function Factorial(N : Integer) return Integer
+        with 
+            Export        => True,
+            Convention    => C,
+            External_Name => "factorial_c";
 
-    function GCD(A, B : Integer) return Integer;
+    function GCD(A, B : Integer) return Integer
+        with 
+            Export        => True,
+            Convention    => C,
+            External_Name => "gcd_c";
 
-    procedure Diophantine(A, B, C : Integer; X, Y : out Integer);
+    procedure Diophantine(A, B, C : Integer; X, Y : out Integer)
+        with 
+            Export        => True,
+            Convention    => C,
+            External_Name => "diophantine_c";
 
 end Recursive;
